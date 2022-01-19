407e78dbea5a9faa20a7a83e4608d887870567f1
Suril Patel <surilsp@gmail.com>
2022-01-18 20:07:12 -0500

    quick select recursion to iterative

--- search/quickselect.py
+++ search/quickselect.py
@@ -7,30 +7,30 @@ def quickselect(array, k):
 
 
 def _quickselect(array, start, end, k):
-    if start > end:
-        return -1
-    pivot_idx = end
-    i = start
-    j = start
-    pivot_value = array[pivot_idx]
-    while j < end:
-        if array[j] > pivot_value:
-            j += 1
+    while start < end:
+        pivot_idx = end
+        i = start
+        j = start
+        pivot_value = array[pivot_idx]
+        while j < end:
+            if array[j] > pivot_value:
+                j += 1
+            else:
+                array[i], array[j] = array[j], array[i]
+                i += 1
+                j += 1
+        array[i], array[pivot_idx] = array[pivot_idx], array[i]
+        pivot_idx = i
+        if pivot_idx == k:
+            return array[pivot_idx]
+        elif k > pivot_idx:
+            start = pivot_idx + 1
         else:
-            array[i], array[j] = array[j], array[i]
-            i += 1
-            j += 1
-    array[i], array[pivot_idx] = array[pivot_idx], array[i]
-    pivot_idx = i
-    if pivot_idx == k:
-        return array[pivot_idx]
-    elif k > pivot_idx:
-        return _quickselect(array, pivot_idx + 1, end, k)
-    else:
-        return _quickselect(array, start, pivot_idx - 1, k)
+            end = pivot_idx - 1
+    return -1
 
 
 array = [8, 5, 2, 9, 7, 6, 3]
-k = 3
+k = 10
 
 print(quickselect(array, k))
