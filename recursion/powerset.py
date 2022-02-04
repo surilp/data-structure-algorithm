08cce3ab27fe1334e19d06040ed6ef972908d834
Suril Patel <surilsp@gmail.com>
2022-02-04 11:22:53 -0500

    powerset using bit operation

--- recursion/powerset.py
+++ recursion/powerset.py
@@ -14,5 +14,23 @@ def _powerset(array, idx, ds, result):
     _powerset(array, idx + 1, ds, result)
 
 
+def powerset_bit(array):
+    array_len = len(array)
+    result = []
+    temp = [''] * array_len
+    for num in range(2 ** array_len):
+        for idx in range(array_len - 1, -1, -1):
+            if is_bit_set(num, idx):
+                temp[idx] = array[idx]
+            else:
+                temp[idx] = ''
+        result.append(''.join(map(str, temp)))
+    return result
 
-print(powerset([1,2,3]))
+
+def is_bit_set(num, pos):
+    return num & (1 << pos) != 0
+
+
+print(powerset([1, 2, 3]))
+print(powerset_bit([1, 2, 3]))
