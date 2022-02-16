aa11442d52c42af5e254b3e1a7075bf20998e268
Suril Patel <surilsp@gmail.com>
2022-02-16 13:54:47 -0500

    tree - node at k distance

---
+++ tree/node_at_distance_k.py
@@ -0,0 +1,63 @@
+'''
+parent pointers
+'''
+
+from tree.Tree import Tree
+from collections import deque, defaultdict
+
+class NodeAtDistanceK(Tree):
+
+    def __init__(self, data):
+        super().__init__(data)
+
+    def nodes_at_distance_k(self, k, target):
+        result = []
+        parent_pointers = self.get_parent_pointers(self.root)
+        self._nodes_at_distance_k(self.root, k, target, result, False)
+        return result
+
+    def _nodes_at_distance_k(self, node, k, target, result, flag):
+        if node:
+            if k == 0:
+                result.append(node.val)
+                return
+
+            if node.val == target or flag:
+                if not flag:
+                    flag = True
+                self._nodes_at_distance_k(node.left, k - 1, target, result, flag)
+                self._nodes_at_distance_k(node.right, k - 1, target, result, flag)
+            else:
+                self._nodes_at_distance_k(node.left, k, target, result, flag)
+                self._nodes_at_distance_k(node.right, k, target, result, flag)
+
+    def get_parent_pointers(self, node):
+        queue = deque()
+        queue.append(node)
+        parent_pointer = {}
+        while queue:
+            current = queue.popleft()
+            if current.left:
+                parent_pointer[current.left] = current
+                queue.append(current.left)
+            if current.right:
+                parent_pointer[current.right] = current
+                queue.append(current.right)
+        return parent_pointer
+
+
+
+
+
+data = {
+    "nodes": [
+        {"id": "1", "left": 5, "right": 1, "value": 3},
+        {"id": "2", "left": 6, "right": 2, "value": 5},
+        {"id": "3", "left": 0, "right": 8, "value": 1},
+        {"id": "3", "left": 7, "right": 4, "value": 2},
+    ],
+    "root": 3
+}
+
+tree = NodeAtDistanceK(data)
+print(tree.nodes_at_distance_k(2, 5))
