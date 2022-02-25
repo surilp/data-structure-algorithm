a0bb28362ae4be80e4225654b11298ff81edbe10
Suril Patel <surilsp@gmail.com>
2022-02-25 16:26:06 -0500

    array - count reverse pairs

--- array/reverse_pairs.py
+++ array/reverse_pairs.py
@@ -10,10 +10,10 @@ class Solution:
         count = 0
         mid = (start + end) // 2
         count += self._reversePairs(nums, start, mid)
-        count += self._reversePairs(nums, mid + 1, start)
+        count += self._reversePairs(nums, mid + 1, end)
         count += self.count_reverse_pairs(nums, start, mid, mid + 1, end)
 
-        self.merge(nums, start, mid, mid + 1, end)
+        self.merge_with_new_array(nums, start, mid, mid + 1, end)
         return count
 
     def merge(self, nums, left_s, left_e, right_s, right_e):
@@ -21,7 +21,56 @@ class Solution:
             if nums[left_s] > nums[right_s]:
                 nums[left_s], nums[right_s] = nums[right_s], nums[left_s]
                 temp = right_s + 1
-                while temp < right_e and nums[temp] < nums[temp - 1]:
+                while temp <= right_e and nums[temp] < nums[temp - 1]:
+                    nums[temp], nums[temp - 1] = nums[temp - 1], nums[temp]
+                    temp += 1
+            left_s += 1
+
+    def merge_with_new_array(self, nums, left_s, left_e, right_s, right_e):
+        l_s = left_s
+        l_e = left_e
+        r_s = right_s
+        r_e = right_e
+
+        temp = [None] * ((left_e - left_s + 1) + (right_e - right_s + 1))
+        temp_i = 0
+        while left_s <= left_e and right_s <= right_e:
+            if nums[left_s] <= nums[right_s]:
+                temp[temp_i] = nums[left_s]
+                left_s += 1
+            else:
+                temp[temp_i] = nums[right_s]
+                right_s += 1
+            temp_i += 1
+        while left_s <= left_e:
+            temp[temp_i] = nums[left_s]
+            temp_i += 1
+            left_s += 1
+        while right_s <= right_e:
+            temp[temp_i] = nums[right_s]
+            temp_i += 1
+            right_s += 1
+
+        left_s = l_s
+        left_e = l_e
+        right_s = r_s
+        right_e = r_e
+
+        temp_i = 0
+        while left_s <= left_e:
+            nums[left_s] = temp[temp_i]
+            temp_i += 1
+            left_s += 1
+        while right_s <= right_e:
+            nums[right_s] = temp[temp_i]
+            temp_i += 1
+            right_s += 1
+
+        while left_s <= left_e:
+            if nums[left_s] > nums[right_s]:
+                nums[left_s], nums[right_s] = nums[right_s], nums[left_s]
+                temp = right_s + 1
+                while temp <= right_e and nums[temp] < nums[temp - 1]:
                     nums[temp], nums[temp - 1] = nums[temp - 1], nums[temp]
                     temp += 1
             left_s += 1
@@ -38,6 +87,10 @@ class Solution:
 
 
 s = Solution()
-nums = [233, 2000000001, 234, 2000000006, 235, 2000000003, 236, 2000000007, 237, 2000000002, 2000000005, 233, 233, 233,
-        233, 233, 2000000004]
+nums = [233, 501, 234, 506, 235, 503, 236, 507, 237, 502, 505, 233, 233, 233,
+        233, 233, 504]
+print(s.reversePairs(nums))
+
+s = Solution()
+nums = [40, 25, 19, 12, 9, 6, 2]
 print(s.reversePairs(nums))
