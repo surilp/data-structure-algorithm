b7919ad80366e850ae6232f9f6bb7cd865efabbc
Suril Patel <surilsp@gmail.com>
2022-03-04 16:37:08 -0500

    graph - shortest distance - bfs 1/0 and dijkstra update

--- graph/the_maze2.py
+++ graph/the_maze2.py
@@ -9,8 +9,6 @@ def shortest_destination(maze, start, destination):
     distance = [[float('inf')] * col_len for _ in range(row_len)]
     distance[start[0]][start[1]] = 0
     heappush(heap, (0, start))
-    visited = set()
-    visited.add((start[0], start[1]))
     while heap:
         dist, node = heappop(heap)
         row, col = node
@@ -20,16 +18,16 @@ def shortest_destination(maze, start, destination):
             new_row = row
             new_col = col
             temp_distance = 0
-            while 0 <= new_row + row_inc <row_len and 0 <= new_col + col_inc < col_len and maze[new_row + row_inc][new_col + col_inc] == 0:
+            while 0 <= new_row + row_inc < row_len and 0 <= new_col + col_inc < col_len and maze[new_row + row_inc][
+                new_col + col_inc] == 0:
                 new_row = new_row + row_inc
                 new_col = new_col + col_inc
                 temp_distance += 1
             new_dist = temp_distance + dist
-            if (new_row, new_col) not in visited and distance[new_row][new_col] > new_dist:
+            if distance[new_row][new_col] > new_dist:
                 distance[new_row][new_col] = new_dist
                 heappush(heap, (new_dist, (new_row, new_col)))
     return -1
-
 
 
 maze = [
@@ -44,7 +42,6 @@ start = [0, 4]
 destination = [4, 4]
 
 print(shortest_destination(maze, start, destination))
-
 
 maze = [
     [0, 0, 1, 0, 0],
